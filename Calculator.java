6275b2f8632972cf88a9412c545e3fb79367836c
sandaralaudepanis <slpanis@paterostechnologicalcollege.edu.ph>
2025-03-18 16:41:27 +0800

    Modified and Final Copy wala nang hahabol bagsak na kung bagsak

--- Calculator.java
+++ Calculator.java
@@ -1,74 +1,60 @@
-
 public class Calculator {
-   
+
     // Group 1: Addition (Members: John, Sarah, Alex)
-    //  Group 1: Addition (Members: Panis, Alabado, Bendoy, Dichoso, Mangui, Sayas, Serrano, Tabilog, Talidong, Villaos) 
+    // Group 1: Addition (Members: Panis, Alabado, Bendoy, Dichoso, Mangui, Sayas,
+    // Serrano, Tabilog, Talidong, Villaos)
 
     public static int add(int a, int b) {
         return a + b;
     }
-<<<<<<< HEAD
-=======
 
-    //Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton, Manuel, Tado, Carino)
- public static int multiply (int a * int b) {
-   return a * b;
-
-
-
->>>>>>> 063d819edf84fec5a87deebe3882b775e8f6455a
-     // Group 8: max (Members: Pianar, Arevalo, Oblepias, GregAgpoon, SamAgpoon, Credo, Baldo, Adriano, Regoroso)
-     public static int max(int a, int b) {
-        // Returns the larger number
-        return (a > b) ? a : b; 
+    // Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton,
+    // Manuel, Tado, Carino)
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 
-    
-    // Group 6: square(int a) – returns the square of a number (Members: Artiaga, Mata, Mago, Respondo, Maglipon, De Belen, Geneblaza, Bondoc, Puzon, Tomenio, Duran)
+    // Group 8: max (Members: Pianar, Arevalo, Oblepias, GregAgpoon, SamAgpoon,
+    // Credo, Baldo, Adriano, Regoroso)
+    public static int max(int a, int b) {
+        // Returns the larger number
+        return (a > b) ? a : b;
+    }
+
+    // Group 6: square(int a) – returns the square of a number (Members: Artiaga,
+    // Mata, Mago, Respondo, Maglipon, De Belen, Geneblaza, Bondoc, Puzon, Tomenio,
+    // Duran)
     public static int square(int a) {
         return a * a;
     }
-  
 
-    // Group 2: Subtract (Members: Lamac, Baluyos, Glipo, Costanilla, Flores, Panganiban, Pagtakhan, Jacinto)  
-    public static int subtract(int a, int b) {  
-    return a - b;  
-      }
+    // Group 2: Subtract (Members: Lamac, Baluyos, Glipo, Costanilla, Flores,
+    // Panganiban, Pagtakhan, Jacinto)
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
 
+    // Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag,
+    // Ringor, Berte)
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 
-    // Group 3 : Product ( Echon, Gabronino, Acal, Villon, Ladera, Emnil, Debuton, Manuel, Tado, Carino)
-        public static int multiply (int a * int b) {
-  	   return a * b;
-	
+    // Group 7: Cube (Members: Tapia, Eugenio, Barcancel, Trampe, Pascualado,
+    // Saulong, Tan, Delig, Epo, De Vera)
+    public static int kyub(int a) {
+        return a * a * a;
+    }
 
+    // Group 5: Modulus (Members: Miranda, Abagat, Garnica, Cababao, De Jesus,
+    // Latagan, Sudicta, Borja, Fernandez)
+    public static int remeynder(int a, int b) {
+        return a % b;
+    }
 
-    //Group 4: Division (Members: Balais, Tabon, Amar, Pajanustan, Garcia, Banaag, Ringor, Berte)
-     public static int divide(int a, int b){
-	return a / b;
-	}
-
-
-     // Group 7: Cube (Members: Tapia, Eugenio, Barcancel, Trampe, Pascualado, Saulong, Tan, Delig, Epo, De Vera)
-     public static  int cube(int a) {
-           return a * a * a;
-        }
-
- // Group 5: Modulus (Members: Miranda, Abagat, Garnica, Cababao, De Jesus, Latagan, Sudicta, Borja, Fernandez)
-   public static int remainder(int a, int b) {
-      return a % b;
-   }
-	
     // Group 10: Check if numbers are equal (Members: Jake, Emily, Ryan)
     public static boolean isEqual(int a, int b) {
         return a == b;
     }
-  
+
 }
-<<<<<<< HEAD
-=======
-
-    
-}
-
-
->>>>>>> 063d819edf84fec5a87deebe3882b775e8f6455a
